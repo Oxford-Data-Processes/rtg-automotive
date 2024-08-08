c507e4e8dfb0c1d84b48e22a740bd18dd552838b
Chris Little <chris.little@sonnedix.com>
2024-08-08 16:22:32 +0100

    Add database

--- infra/main.tf
+++ infra/main.tf
@@ -15,3 +15,46 @@ provider "aws" {
   profile = "speedsheet-management"
   region  = "eu-west-2"
 }
+
+resource "aws_db_instance" "rtg_automotive_db" {
+  identifier           = "${var.project}-db"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  db_name              = "rtg_automotive"
+  username             = "rtg_admin"
+  password             = "rtg_password_123!"  # Note: Use a more secure password in production
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+
+  tags = {
+    Name    = "${var.project}-db"
+    Project = var.project
+  }
+}
+
+output "database_endpoint" {
+  description = "The connection endpoint for the database"
+  value       = aws_db_instance.rtg_automotive_db.endpoint
+}
+
+output "database_name" {
+  description = "The name of the database"
+  value       = aws_db_instance.rtg_automotive_db.db_name
+}
+
+output "database_username" {
+  description = "The master username for the database"
+  value       = aws_db_instance.rtg_automotive_db.username
+}
+
+# Note: Never output the database password in a real-world scenario
+# This is just for demonstration purposes
+output "database_password" {
+  description = "The master password for the database"
+  value       = aws_db_instance.rtg_automotive_db.password
+  sensitive   = true
+}
+
