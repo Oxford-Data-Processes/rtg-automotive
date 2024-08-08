0d6618fee3e2c8f70bbbbb6697ed06ef58fc133c
Chris Little <chris.little@sonnedix.com>
2024-08-08 15:52:53 +0100

    Test new table

--- infra/main.tf
+++ infra/main.tf
@@ -17,3 +17,20 @@ provider "aws" {
 }
 
 
+# Test resource: DynamoDB table
+resource "aws_dynamodb_table" "test_table" {
+  name           = "${var.project}-test-table"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "${var.project} Test Table"
+    Environment = "Test"
+    Project     = var.project
+  }
+}
