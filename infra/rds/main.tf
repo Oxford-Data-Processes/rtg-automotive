9b148bb1b9863318c6148deb135a9d9905b08855
Chris Little <chris.little@sonnedix.com>
2024-08-08 13:24:56 +0100

    Add rds

---
+++ infra/rds/main.tf
@@ -0,0 +1,47 @@
+# main.tf
+resource "aws_security_group" "rds_sg" {
+  name        = "rds_sg"
+  description = "Allow inbound traffic to RDS instance"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Adjust as needed for security
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_subnet_group" "default" {
+  name       = "my-db-subnet-group"
+  subnet_ids = var.subnet_ids
+
+  tags = {
+    Name = "MyDBSubnetGroup"
+  }
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = var.allocated_storage
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = var.db_instance_class
+  db_name              = var.db_name
+  username             = var.db_username
+  password             = var.db_password
+  db_subnet_group_name = aws_db_subnet_group.default.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  skip_final_snapshot  = true
+
+  tags = {
+    Name = "MyDBInstance"
+  }
+}
---
+++ infra/rds/variables.tf
@@ -0,0 +1,41 @@
+# variables.tf
+variable "db_name" {
+  description = "The name of the database"
+  type        = string
+  default     = "mydatabase"
+}
+
+variable "db_username" {
+  description = "The database admin username"
+  type        = string
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "The database admin password"
+  type        = string
+  default     = "yourpassword"
+  sensitive   = true
+}
+
+variable "db_instance_class" {
+  description = "The instance type of the RDS instance"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "allocated_storage" {
+  description = "The allocated storage in GBs"
+  type        = number
+  default     = 20
+}
+
+variable "vpc_id" {
+  description = "The VPC ID where the RDS instance will be deployed"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "The Subnet IDs for the RDS instance"
+  type        = list(string)
+}
