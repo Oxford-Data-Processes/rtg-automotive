ca9b8bfd552e6ba5a331fd366a9bacff93104bd2
Chris Little <chris.little@sonnedix.com>
2024-08-08 16:58:23 +0100

    Checkpoint

--- infra/main.tf
+++ infra/main.tf
@@ -25,7 +25,7 @@ resource "aws_db_instance" "rtg_automotive_db" {
   storage_type         = "gp2"
   db_name              = "rtg_automotive"
   username             = "rtg_admin"
-  password             = "rtg_password_123!"  # Note: Use a more secure password in production
+  password             = "password" # Note: Use a more secure password in production
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
 
@@ -33,28 +33,5 @@ resource "aws_db_instance" "rtg_automotive_db" {
     Name    = "${var.project}-db"
     Project = var.project
   }
-}
-
-output "database_endpoint" {
-  description = "The connection endpoint for the database"
-  value       = aws_db_instance.rtg_automotive_db.endpoint
-}
-
-output "database_name" {
-  description = "The name of the database"
-  value       = aws_db_instance.rtg_automotive_db.db_name
-}
-
-output "database_username" {
-  description = "The master username for the database"
-  value       = aws_db_instance.rtg_automotive_db.username
-}
-
-# Note: Never output the database password in a real-world scenario
-# This is just for demonstration purposes
-output "database_password" {
-  description = "The master password for the database"
-  value       = aws_db_instance.rtg_automotive_db.password
-  sensitive   = true
 }
 
