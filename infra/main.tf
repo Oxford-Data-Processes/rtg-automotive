606cb93b56c4a8a556adb3eece2da0bfdff5f467
Chris Little <chris.little@sonnedix.com>
2024-08-09 12:08:45 +0100

    Add permissions

--- infra/main.tf
+++ infra/main.tf
@@ -16,6 +16,30 @@ provider "aws" {
   region  = "eu-west-2"
 }
 
+resource "aws_security_group" "rds_sg" {
+  name        = "${var.project}-rds-sg"
+  description = "Allow MySQL access from any IP"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow access from any IP address
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "${var.project}-rds-sg"
+    Project = var.project
+  }
+}
+
 resource "aws_db_instance" "rtg_automotive_db" {
   identifier           = "${var.project}-db"
   engine               = "mysql"
@@ -28,9 +52,10 @@ resource "aws_db_instance" "rtg_automotive_db" {
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
 
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
   tags = {
     Name    = "${var.project}-db"
     Project = var.project
   }
 }
-
