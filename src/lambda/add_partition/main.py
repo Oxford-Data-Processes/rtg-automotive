37295fee5d8b3bba5a7bd8159a36b2514de7dc77
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-09-25 17:29:09 +0100

    Fixing multiple filters issue

--- src/lambda/add_partition/main.py
+++ src/lambda/add_partition/main.py
@@ -103,7 +103,15 @@ def lambda_handler(event, context):
     database_name = "rtg_automotive"
     table_name = get_table_name(object_key)
 
-    response = add_partition_to_glue(
-        glue_client, database_name, table_name, bucket_name, partition_values, logger
-    )
-    return response
+    if table_name in ["supplier_stock", "store"]:
+        response = add_partition_to_glue(
+            glue_client,
+            database_name,
+            table_name,
+            bucket_name,
+            partition_values,
+            logger,
+        )
+        return response
+
+    return {"statusCode": 200, "body": json.dumps("No partition added!")}
