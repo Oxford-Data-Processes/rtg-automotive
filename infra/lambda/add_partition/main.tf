80e7dc0f02aab20309d7f2ae7c0adfa13b020136
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-09-25 15:55:25 +0100

    Use terraform to create trigger

--- infra/lambda/add_partition/main.tf
+++ infra/lambda/add_partition/main.tf
@@ -18,3 +18,24 @@ resource "aws_iam_role_policy_attachment" "lambda_iam_to_policy_attachment" {
   policy_arn = aws_iam_policy.inline_policy.arn
   role       = aws_iam_role.lambda_iam.name
 }
+
+resource "aws_lambda_event_source_mapping" "s3_trigger" {
+  event_source_arn = "arn:aws:s3:::${var.project}-bucket-${var.aws_account_id}"
+  function_name = "arn:aws:lambda:${var.region}:${var.aws_account_id}:function:${var.project}-dev-${local.service_name}"
+  starting_position = "LATEST"
+
+  batch_size = 1
+
+  filter {
+    key {
+      filter_rules {
+        name  = "prefix"
+        value = "supplier_stock/"
+      }
+      filter_rules {
+        name  = "suffix"
+        value = ".parquet"
+      }
+    }
+  }
+}
