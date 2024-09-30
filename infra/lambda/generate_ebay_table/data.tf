fa963a947bdd6831e8286f7307e121bb623fcfba
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-09-30 12:16:52 +0100

    Add permissions 5

--- infra/lambda/generate_ebay_table/data.tf
+++ infra/lambda/generate_ebay_table/data.tf
@@ -43,7 +43,8 @@ data "aws_iam_policy_document" "lambda_policy" {
     effect = "Allow"
     actions = [
       "athena:StartQueryExecution",  // Added permission for Athena
-      "athena:GetQueryExecution"      // Added permission for GetQueryExecution
+      "athena:GetQueryExecution",      // Added permission for GetQueryExecution
+      "athena:GetQueryResults"         // Added permission for GetQueryResults
     ]
     resources = [
       "arn:aws:athena:${var.aws_region}:${var.aws_account_id}:workgroup/*"  // Added resource for Athena
