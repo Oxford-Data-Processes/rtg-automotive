ac93a152ad844f5d770a1631015b47a31853b7a2
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-09-30 19:47:40 +0100

    Fix permissions 2

--- src/lambda/process_stock_feed/main.py
+++ src/lambda/process_stock_feed/main.py
@@ -206,7 +206,10 @@ def send_sns_notification(message, AWS_ACCOUNT_ID):
     sns_client = boto3.client("sns")
     topic_arn = f"arn:aws:sns:eu-west-2:{AWS_ACCOUNT_ID}:rtg-automotive-stock-notifications.fifo"
     sns_client.publish(
-        TopicArn=topic_arn, Message=message, Subject="Stock Feed Processed"
+        TopicArn=topic_arn,
+        Message=message,
+        Subject="Stock Feed Processed",
+        MessageGroupId="groupId",
     )
 
 
