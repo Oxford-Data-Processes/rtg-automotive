3cff1ddb7d18af98a38cd9e1a85a9e51b8857657
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-10-16 20:45:19 +0100

    Generate ebay table lambda

--- src/lambda/add_partition/main.py
+++ src/lambda/add_partition/main.py
@@ -57,7 +57,6 @@ def lambda_handler(event, context):
             return response
         except Exception as e:
             logger.error(f"Error adding partition: {e}")
-            response = {"statusCode": 500, "body": json.dumps("Error adding partition")}
-        return response
+            raise e
     else:
         return {"statusCode": 200, "body": json.dumps("Not a partitionable table")}
--- src/lambda/generate_ebay_table/main.py
+++ src/lambda/generate_ebay_table/main.py
@@ -1,7 +1,7 @@
 import json
-import boto3
 import logging
 import os
+from aws_utils import athena
 
 # Set up logging
 logger = logging.getLogger()
@@ -9,38 +9,23 @@ logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event, context):
-    # Initialize a session using Boto3
-    session = boto3.Session()
-    athena_client = session.client("athena")
+    athena_handler = athena.AthenaHandler()
 
     current_directory = os.path.dirname(os.path.abspath(__file__))
 
     with open(os.path.join(current_directory, "generate_ebay_table.sql"), "r") as file:
         query = file.read()
 
-    # Define the parameters for the query execution
-    response = athena_client.start_query_execution(
-        QueryString=query,
-        QueryExecutionContext={"Database": "rtg_automotive"},
-        WorkGroup="rtg-automotive-workgroup",
-    )
-
     logger.info(f"Query: {query}")
-    query_execution_id = response["QueryExecutionId"]
-    # Wait for the query to complete
-    while True:
-        query_status = athena_client.get_query_execution(
-            QueryExecutionId=query_execution_id
-        )
-        status = query_status["QueryExecution"]["Status"]["State"]
-        if status in ["SUCCEEDED", "FAILED", "CANCELLED"]:
-            break
+    results = athena_handler.run_query(query)
 
-    if status == "SUCCEEDED":
-        # Get the results
-        results = athena_client.get_query_results(QueryExecutionId=query_execution_id)
+    if results["ResultSet"]["Rows"]:
         logger.info(f"Query results: {results['ResultSet']['Rows'][:5]}")
     else:
-        logger.error(f"Query failed with status: {status}")
-
+        logger.error(
+            f"Query failed with status: {results['ResultSet']['Status']['State']}"
+        )
+        raise Exception(
+            f"Query failed with status: {results['ResultSet']['Status']['State']}"
+        )
     return {"statusCode": 200, "body": json.dumps("Query executed successfully!")}
