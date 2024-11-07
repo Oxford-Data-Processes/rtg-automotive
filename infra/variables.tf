7009bc6a78c0ed5024e5fea7aac66e6910f5c88d
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-11-07 09:55:44 +0000

    Add variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,10 +6,22 @@ variable "aws_region" {
   type = string
 }
 
+variable "stage" {
+  type = string
+}
+
 variable "project" {
   type = string
 }
 
-variable "stage" {
+variable "aws_access_key_id_admin" {
+  type = string
+}
+
+variable "aws_secret_access_key_admin" {
+  type = string
+}
+
+variable "version_number" {
   type = string
 }
