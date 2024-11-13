f8989085c00191be9cad489e211762470e9f39e2
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-11-13 16:06:45 +0000

    Deploy api 5

--- infra/main.tf
+++ infra/main.tf
@@ -21,6 +21,34 @@ module "s3_bucket" {
   source         = "./s3_bucket"
 }
 
+module "sqs" {
+  project        = var.project
+  source         = "./sqs"
+}
+
+module "rds" {
+  aws_account_id = var.aws_account_id
+  project        = var.project
+  source = "./rds"
+}
+
+module "eventbridge" {
+  aws_account_id = var.aws_account_id
+  aws_region     = var.aws_region
+  project        = var.project
+  source         = "./eventbridge"
+}
+
+module "api_gateway" {
+  aws_account_id              = var.aws_account_id
+  aws_region                  = var.aws_region
+  stage                       = var.stage
+  project                     = var.project
+  aws_access_key_id_admin     = var.aws_access_key_id_admin
+  aws_secret_access_key_admin = var.aws_secret_access_key_admin
+  version_number              = var.version_number
+  source                      = "./api_gateway"
+}
 
 module "lambda" {
   aws_account_id              = var.aws_account_id
@@ -31,12 +59,4 @@ module "lambda" {
   aws_secret_access_key_admin = var.aws_secret_access_key_admin
   version_number              = var.version_number
   source                      = "./lambda"
-}
-
-module "sqs" {
-  aws_account_id = var.aws_account_id
-  project        = var.project
-  stage          = var.stage
-  aws_region     = var.aws_region
-  source         = "./sqs"
 }
