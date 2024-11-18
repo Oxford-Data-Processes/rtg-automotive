78e1d0e0b6c7b97bef430b8f56253a2cd07a7b1f
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-11-18 11:46:30 +0000

    Update generate ebay table lambda

--- src/aws_lambda/generate_ebay_table/generate_ebay_table.sql
+++ src/aws_lambda/generate_ebay_table/generate_ebay_table.sql
@@ -8,34 +8,35 @@ CREATE TABLE ebay (
     updated_date VARCHAR(255),
     ebay_store VARCHAR(255),
     supplier VARCHAR(255)
-) AS
-SELECT 
+) AS (
+SELECT
     sd.ebay_store,
     rd.supplier,
     rd.custom_label,
     rd.quantity,
     COALESCE(
-        MAX(CASE WHEN rn = 1 THEN rd.quantity END) - 
-        MAX(CASE WHEN rn = 2 THEN rd.quantity END), 
+        MAX(CASE WHEN rn = 1 THEN rd.quantity END) -
+        MAX(CASE WHEN rn = 2 THEN rd.quantity END),
         MAX(CASE WHEN rn = 1 THEN rd.quantity END)
     ) AS quantity_delta,
     MAX(rd.updated_date) AS updated_date,
     sd.item_id
-FROM 
+FROM
     supplier_stock ps
-LEFT JOIN 
+LEFT JOIN
     store sd ON ps.custom_label = sd.custom_label
-LEFT JOIN 
+LEFT JOIN
     (
-        SELECT 
+        SELECT
             ps.part_number,
             ps.supplier,
             ps.quantity,
             ps.updated_date,
             ps.custom_label,
             ROW_NUMBER() OVER (PARTITION BY ps.supplier, ps.custom_label ORDER BY ps.updated_date DESC) AS rn
-        FROM 
+        FROM
             supplier_stock ps
     ) rd ON ps.custom_label = rd.custom_label
-GROUP BY 
-    sd.item_id, rd.supplier, sd.ebay_store, rd.quantity, rd.custom_label;
+GROUP BY
+    sd.item_id, rd.supplier, sd.ebay_store, rd.quantity, rd.custom_label
+);
--- src/aws_lambda/generate_ebay_table/main.py
+++ src/aws_lambda/generate_ebay_table/main.py
@@ -1,33 +1,49 @@
 import json
 import logging
 import os
-from aws_utils import athena, iam
-from aws_lambda.api.models.pydantic_models import PROJECT
+
+from aws_utils import iam
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+
+def create_database_session() -> sessionmaker:
+    engine = create_engine(
+        "mysql+mysqlconnector://admin:password@rtg-automotive-mysql.c14oos6givty.eu-west-2.rds.amazonaws.com/rtg_automotive"
+    )
+    return sessionmaker(bind=engine)
+
 
 iam.get_aws_credentials(os.environ)
 
 
 def lambda_handler(event, context):
-    project_bucket_name = f"{PROJECT}-bucket-{os.environ["AWS_ACCOUNT_ID"]}"
-    athena_handler = athena.AthenaHandler(
-        database=PROJECT,
-        workgroup=f"{PROJECT}-workgroup",
-        output_bucket=project_bucket_name,
-    )
-
+    session = create_database_session()()
     current_directory = os.path.dirname(os.path.abspath(__file__))
 
     with open(os.path.join(current_directory, "generate_ebay_table.sql"), "r") as file:
-        query = file.read()
+        queries = file.read().split(
+            ";"
+        )  # Split the SQL file into individual statements
 
-    logger.info(f"Query: {query}")
-    results = athena_handler.run_query(query)
+    logger.info(f"Queries: {queries}")
 
-    if len(results) > 0:
-        logger.info(f"Query results - columns: {results[0]} values: {results[1:6]}")
-    else:
-        logger.error(f"Query failed: {query}")
-        raise Exception(f"Query failed: {query}")
-    return {"statusCode": 200, "body": json.dumps("Query executed successfully!")}
+    try:
+        for query in queries:
+            if query.strip():  # Check if the query is not empty
+                session.execute(text(query))  # Execute each query separately
+
+        session.commit()  # Commit the transaction after executing all queries
+
+        logger.info("All queries executed successfully.")
+    except Exception as e:
+        logger.error(f"Error executing queries: {str(e)}")
+        session.rollback()  # Rollback in case of error
+        raise e
+    finally:
+        session.close()
+
+    return {"statusCode": 200, "body": json.dumps("Queries executed successfully!")}
