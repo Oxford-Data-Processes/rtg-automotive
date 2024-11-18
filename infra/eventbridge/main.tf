d6b7b872ce86661d3a2f8c213dbd207c6658d286
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-11-18 13:13:39 +0000

    Add generate ebay table lambda trigger

--- infra/eventbridge/main.tf
+++ infra/eventbridge/main.tf
@@ -9,3 +9,16 @@ module "process_stock_feed_lambda_eventbridge" {
   aws_region     = var.aws_region
   project        = var.project
 }
+
+
+module "generate_ebay_table_lambda_eventbridge" {
+  source               = "git::https://github.com/Oxford-Data-Processes/terraform.git//modules/eventbridge"
+  lambda_function_name = "generate-ebay-table-lambda"
+  event_pattern = jsonencode({
+    "source" : ["com.oxforddataprocesses"],
+    "detail-type" : ["RtgAutomotiveGenerateEbayTable"]
+  })
+  aws_account_id = var.aws_account_id
+  aws_region     = var.aws_region
+  project        = var.project
+}
