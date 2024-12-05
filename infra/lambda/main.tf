7f10e5ce2a4f124e780c80c187014135c3883561
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-12-05 15:30:53 +0000

    Save

--- infra/lambda/main.tf
+++ infra/lambda/main.tf
@@ -30,3 +30,25 @@ module "generate_ebay_table" {
   aws_secret_access_key_admin = var.aws_secret_access_key_admin
   version_number              = var.version_number
 }
+
+module "run_sql_query" {
+  source                      = "git::https://github.com/Oxford-Data-Processes/terraform.git//modules/lambda"
+  lambda_function_name        = "${var.project}-run-sql-query-lambda"
+  aws_account_id              = var.aws_account_id
+  aws_region                  = var.aws_region
+  stage                       = var.stage
+  aws_access_key_id_admin     = var.aws_access_key_id_admin
+  aws_secret_access_key_admin = var.aws_secret_access_key_admin
+  version_number              = var.version_number
+}
+
+module "create_parquet" {
+  source                      = "git::https://github.com/Oxford-Data-Processes/terraform.git//modules/lambda"
+  lambda_function_name        = "${var.project}-create-parquet-lambda"
+  aws_account_id              = var.aws_account_id
+  aws_region                  = var.aws_region
+  stage                       = var.stage
+  aws_access_key_id_admin     = var.aws_access_key_id_admin
+  aws_secret_access_key_admin = var.aws_secret_access_key_admin
+  version_number              = var.version_number
+}
