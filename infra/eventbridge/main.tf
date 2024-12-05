687331c28c641bdb7b8e85540264804012c12b1d
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-12-05 15:47:41 +0000

    Add eventbridge triggers

--- infra/eventbridge/main.tf
+++ infra/eventbridge/main.tf
@@ -22,3 +22,28 @@ module "generate_ebay_table_lambda_eventbridge" {
   aws_region     = var.aws_region
   project        = var.project
 }
+
+module "run_sql_query_lambda_eventbridge" {
+  source               = "git::https://github.com/Oxford-Data-Processes/terraform.git//modules/eventbridge"
+  lambda_function_name = "run-sql-query-lambda"
+  event_pattern = jsonencode({
+    "source" : ["com.oxforddataprocesses"],
+    "detail-type" : ["RtgAutomotiveRunSQLQuery"]
+  })
+  aws_account_id = var.aws_account_id
+  aws_region     = var.aws_region
+  project        = var.project
+}
+
+
+module "create_parquet_lambda_eventbridge" {
+  source               = "git::https://github.com/Oxford-Data-Processes/terraform.git//modules/eventbridge"
+  lambda_function_name = "create-parquet-lambda"
+  event_pattern = jsonencode({
+    "source" : ["com.oxforddataprocesses"],
+    "detail-type" : ["RtgAutomotiveCreateParquet"]
+  })
+  aws_account_id = var.aws_account_id
+  aws_region     = var.aws_region
+  project        = var.project
+}
